22862165f721dd7ab4c208e741c24c19443b3653
BenjaminLin808 <benjaminlin808@gmail.com>
2023-03-27 22:06:34 -0700

    Day20, Day22, Day24, Day25, and Day26

---
+++ Day-24-File-System/Mail_Merge/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+#     letter_contents = letter_file.read()
+#     for name in names:
+#         new_letter = letter_contents.replace(PLACEHOLDER, name)
+#         print(new_letter)
---
+++ Day-25-CSV-Files/Learning/main.py
@@ -0,0 +1,31 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+# import csv
+
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+           
+#     print(temperatures) 
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+# print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+print(data["temp"].mean())
+print(data["temp"].max())
+
+print(data["condition"])
+print(data.condition)
